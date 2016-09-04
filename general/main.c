043615c092d96037728abdf8c0004f4ce8a3986f
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 13:40:06 -0700

    Added template for new programs

---
+++ general/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define NUM_ARGS 2
+
+int main(int argc, char* argv[])
+{
+
+    // Too few arguments
+    if (argc < NUM_ARGS) {
+        puts("Not enougb arguments");
+        return -1;
+    }
+
+    // Too many arguments
+    if (argc > NUM_ARGS) {
+        puts("Too many arguments");
+        return -1;
+    }
+
+    char garbage[32];
+    int intArg;
+
+    // Argument not only a number
+    if (sscanf(argv[1], "%i%s", &intArg, garbage) != 1) {
+        puts("Argument improperly formatted");
+        return -1;
+    }
+
+    // Argument a non-natural number
+    if (intArg < 1) {
+        puts("Number must be greater than zero");
+        return -1;
+    }
+
+    // Put line of code here that calls funtion from problem
+
+    return 0;
+}
