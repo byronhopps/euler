50cb8cbf78bde2e1f8f0c3f2a8b0f3296a0ceb4c
Byron Hopps <byron.hopps@gmail.com>
2016-09-03 17:21:45 -0700

    Added a command line argument for the total

--- 31-coinSums/main.c
+++ 31-coinSums/main.c
@@ -4,7 +4,23 @@
 int main (int argc, char* argv[])
 {
 
-    printf("There are %i ways to make 200p\n", countCoins(0));
+    char garbage[32];
+    int total;
+
+    if (argc < 2) {
+        printf("Too few arguments\n");
+        return -1;
+
+    } else if (argc > 2) {
+        printf("Too many arguments\n");
+        return -1;
+
+    } else if (sscanf(argv[1], "%i%s", &total, garbage) != 1) {
+        printf("Argument improperly formatted\n");
+        return -1;
+    }
+
+    printf("There are %i ways to make %ip\n", countCoins(0), total);
 
     return 0;
 }
