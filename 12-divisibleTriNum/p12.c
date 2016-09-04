fcea6d3802080fdd19aaddce0fc9a17c5edbc714
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 14:28:46 -0700

    Implemented countDivisors

--- 12-divisibleTriNum/p12.c
+++ 12-divisibleTriNum/p12.c
@@ -1,7 +1,27 @@
 #include "p12.h"
+#include <math.h>
 
 // Returns the nth triangle number
 unsigned int triNum(unsigned int n)
 {
     return n * (n + 1) / 2;
+}
+
+// Returns the number of divisors n has
+unsigned int countDivisors(unsigned int n)
+{
+    int factors = 0;
+
+    for (int i = 0; i <= sqrtf((float)n); i++) {
+
+        if (n % i != 0)
+            continue;
+
+        if (n == i*i)
+            factors += 1;
+        else
+            factors += 2;
+    }
+
+    return factors;
 }
