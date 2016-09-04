c2e83ef549961eb70c2407b2b1c5f1b53fd8c8aa
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 16:36:12 -0700

    Initial setup

---
+++ 11-gridProduct/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <time.h>
+#include "p11.h"
+
+#define NUM_ARGS 2
+
+int main(int argc, char* argv[])
+{
+
+    // Too few arguments
+    if (argc < NUM_ARGS) {
+        puts("Not enougb arguments");
+        return -1;
+    }
+
+    // Too many arguments
+    if (argc > NUM_ARGS) {
+        puts("Too many arguments");
+        return -1;
+    }
+
+    char garbage[32];
+    int intArg;
+
+    // Argument not only a number
+    if (sscanf(argv[1], "%i%s", &intArg, garbage) != 1) {
+        puts("Argument improperly formatted");
+        return -1;
+    }
+
+    // Argument a non-natural number
+    if (intArg < 1) {
+        puts("Number must be greater than zero");
+        return -1;
+    }
+
+    clock_t start, end;
+    double cpuTimeUsed;
+
+    start = clock();
+    // Put line of code here that calls funtion from problem
+    end = clock();
+
+    cpuTimeUsed = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    // printf result of program execution
+    printf("Program executed in %f seconds\n", cpuTimeUsed);
+
+    return 0;
+}
