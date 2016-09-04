c4a13557185d2b9d983bfc5f74cdfa1b5eba20cf
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 14:57:22 -0700

    Finished countDivisors

--- 12-divisibleTriNum/p12.c
+++ 12-divisibleTriNum/p12.c
@@ -12,7 +12,7 @@ unsigned int countDivisors(unsigned int n)
 {
     int factors = 0;
 
-    for (int i = 0; i <= sqrtf((float)n); i++) {
+    for (int i = 1; i <= sqrtf((float)n); i++) {
 
         if (n % i != 0)
             continue;
