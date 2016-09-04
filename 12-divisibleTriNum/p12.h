cb08a3778db87f4832792936b71b5f35fa6a96b9
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 13:55:43 -0700

    Added initial function prototypes

---
+++ 12-divisibleTriNum/p12.h
@@ -0,0 +1,8 @@
+// Returns the nth triangle number
+unsigned int triNum(unsigned int n);
+
+// Returns the number of divisors n has
+unsigned int countDivisors(unsigned int n);
+
+// Returns the first triangular number with n divisors
+unsigned int getTriNumByDivisors(unsigned int n);
