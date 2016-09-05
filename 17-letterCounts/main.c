cfc76b18571e0ef014e1d15abe8ccc8cbd9db288
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 23:37:24 -0700

    Setup args of main.c

--- 17-letterCounts/main.c
+++ 17-letterCounts/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <time.h>
+#include "p17.h"
 
-#define NUM_ARGS 2
+#define NUM_ARGS 3
 
 int main(int argc, char* argv[])
 {
@@ -19,17 +20,24 @@ int main(int argc, char* argv[])
     }
 
     char garbage[32];
-    int intArg;
+    int min, max;
 
-    // Argument not only a number
-    if (sscanf(argv[1], "%i%s", &intArg, garbage) != 1) {
-        puts("Argument improperly formatted");
+    // Ensures that the arguments only contain a number
+    if (sscanf(argv[1], "%i%s", &min, garbage) != 1) {
+        puts("Argument 1 improperly formatted");
         return -1;
-    }
 
-    // Argument a non-natural number
-    if (intArg < 1) {
-        puts("Number must be greater than zero");
+    } else if (sscanf(argv[2], "%i%s", &max, garbage) != 1) {
+        puts("Argument 2 improperly formatted");
+        return -1;
+
+    // Arguments must be greater than zero
+    } else if (min < 1 || max < 1) {
+        puts("Bounds must be greater than zero");
+        return -1;
+
+    } else if (min > max) {
+        puts("Upper bound must be greater than lower bound");
         return -1;
     }
 
@@ -37,12 +45,13 @@ int main(int argc, char* argv[])
     double cpuTimeUsed;
 
     start = clock();
-    // Put line of code here that calls funtion from problem
+    int result = getRangedCharCount(min, max);
     end = clock();
 
     cpuTimeUsed = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-    // printf result of program execution
+    printf("%i characters are needed to print out all numbers from "
+            "%i to %i\n", result, min, max);
     printf("Program executed in %f seconds\n", cpuTimeUsed);
 
     return 0;
