38405364e8d6490df2fcb2b901dc686a87e048d2
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 17:35:54 -0700

    Added checking for vertical and horizontal as well

--- 11-gridProduct/p11.c
+++ 11-gridProduct/p11.c
@@ -6,6 +6,8 @@
 int largestDiagProduct()
 {
 
+    // grid[i][j]
+    // i is x-axis, j is y-axis
     int grid[20][20];
 
     importGrid(grid);
@@ -44,6 +46,39 @@ int largestDiagProduct()
                 maxProduct = product;
         }
     }
+
+    // Loop over all valid starting points
+    // for a horizontal
+    for (int i = 0; i < 17; i++) {
+        for (int j = 0; j < 20; j++) {
+
+            // Check the / diagonal
+            product = 1;
+            for (int k = 0; k < 4; k++) {
+                product *= grid[i+k][j];
+            }
+
+            if (product > maxProduct)
+                maxProduct = product;
+        }
+    }
+
+    // Loop over all valid starting points
+    // for a vertical
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 17; j++) {
+
+            // Check the / diagonal
+            product = 1;
+            for (int k = 0; k < 4; k++) {
+                product *= grid[i][j+k];
+            }
+
+            if (product > maxProduct)
+                maxProduct = product;
+        }
+    }
+
 
     return maxProduct;
 }
