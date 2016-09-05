58150194d6c583d2abc9b06537e1481c3c6cf8d0
Byron Hopps <byron.hopps@gmail.com>
2016-09-04 23:37:43 -0700

    Setup function prototypes

---
+++ 17-letterCounts/p17.h
@@ -0,0 +1,6 @@
+// Returns the number of character used to spell out a nubmer
+int numCharCount(int num);
+
+// Returns the number of characters needed to print all numbers
+// from min to max, inclusive
+int getRangedCharCount(int min, int max);
