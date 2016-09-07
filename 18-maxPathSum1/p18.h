b4d409ff671fd6d11d513075d2274255d8996691
Byron Hopps <byron.hopps@gmail.com>
2016-09-06 21:29:44 -0700

    Solved problem 18

--- 18-maxPathSum1/p18.h
+++ 18-maxPathSum1/p18.h
@@ -1,7 +1,7 @@
 #include <limits.h>
 #include <stdio.h>
 
-#define NUM_ROWS 4
+#define NUM_ROWS 15
 
 int getMaxPathSum(FILE* inputFile);
 void readFile(FILE* inputFile, int array[NUM_ROWS][NUM_ROWS]);
