41bace8f1962e6874d5478caac61c134078af4f9
Byron Hopps <byron.hopps@gmail.com>
2017-01-08 01:24:33 -0600

    Added psuedocode of basic program structure

---
+++ 96-sudoku/p96.c
@@ -0,0 +1,6 @@
+int solveProblem() {
+    // For all 50 puzzles:
+        // Read puzzle to array
+        // Solve puzzle
+        // Add sum of the 3 numbers in the top left to accumulating sum 
+}
