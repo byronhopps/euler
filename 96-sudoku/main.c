a2b8947fd674fc9965aa7123ab112c0f39b4fa6e
Byron Hopps <byron.hopps@gmail.com>
2017-01-10 00:25:12 -0600

    Added call to function to solve problem

--- 96-sudoku/main.c
+++ 96-sudoku/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include "p96.h"
 
 #define NUM_ARGS 2
 
@@ -18,32 +19,18 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    char garbage[32];
-    int intArg;
-
-    // Argument not only a number
-    if (sscanf(argv[1], "%i%s", &intArg, garbage) != 1) {
-        puts("Argument improperly formatted");
-        return -1;
-    }
-
-    // Argument a non-natural number
-    if (intArg < 1) {
-        puts("Number must be greater than zero");
-        return -1;
-    }
-
     clock_t start, end;
     double cpuTimeUsed;
 
     start = clock();
-    // Put line of code here that calls funtion from problem
+    int result = solveProblem96(argv[1]);
     end = clock();
 
     cpuTimeUsed = ((double)(end - start)) / CLOCKS_PER_SEC;
 
     // printf result of program execution
     printf("Program executed in %f seconds\n", cpuTimeUsed);
+    printf("Sum of items in sudoku puzzle is %i\n", result);
 
     return 0;
 }
