8a6132c7bf3fc462eeb90d38a42f6698911a8193
Byron Hopps <byron.hopps@gmail.com>
2017-01-10 13:09:26 -0600

    Implemented problem solving logic

--- 08-largestProduct/p8.py
+++ 08-largestProduct/p8.py
@@ -20,3 +20,26 @@ with open(filePath, 'r') as file:
 
 # Remove newlines from string
 numString = numString.translate(None, "\n")
+
+# Verify file formatting
+if not numString.isdigit():
+    sys.exit("Invalid file formatting")
+
+# Loop over the valid consecutive 13 digit substring of numString
+i = 0; maxProduct = 0
+while i+13 <= len(numString):
+
+    # Create a string that is a subset of the main numString
+    shortStr = numString[i:i+13]
+
+    # The shortStr must be 13 digits long
+    assert len(shortStr) == 13
+
+    # Multiply the digits in shortStr
+    product = 1
+    for idx, char in enumerate(shortStr):
+        product *= char.int()
+
+    # Find the largest product
+    if product > maxProduct:
+        maxProduct = product 
