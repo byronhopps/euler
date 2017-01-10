325b67b55ed563db4920405ec36447585ad99acc
Byron Hopps <byron.hopps@gmail.com>
2017-01-10 16:04:48 -0600

    Added string comparison function

--- 22-nameScores/p22.c
+++ 22-nameScores/p22.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "p22.h"
 
 #define NAME_ARRAY_SIZE 6000
 #define MAX_NAME_LENGTH 32
@@ -17,10 +18,24 @@ unsigned long long int getNameScores(char* filePath)
 
     // Loop through all names in file
     while (fscanf(namesFile, "\"%s\"", buffer) != EOF) {
+
+        // Add pointer to string to nameArray, and copy string to memory
         nameArray[nameCount] = (char*)malloc(sizeof(char[MAX_NAME_LENGTH]));
         strcpy(nameArray[nameCount], buffer);
+
+        // Increment number of names imported
         nameCount++;
     }
 
+    // Sort nameArray in alphabetic order
+    qsort(nameArray, nameCount, sizeof(char*), compareNames);
+
+    // TODO: free all the malloc-ed memory before returning
+
     return 0;
+}
+
+int compareNames(const void *a, const void* b)
+{
+    return strcmp(a, b);
 }
--- 22-nameScores/p22.h
+++ 22-nameScores/p22.h
@@ -1,1 +1,3 @@
 unsigned long long int getNameScores(char* filePath);
+
+int compareNames(const void *a, const void* b);
